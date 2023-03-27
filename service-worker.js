53cd40c7bd7ea1aeaf72a00dc9a46b4b54906e6e
Appu S Babu <appu.sbabu@inseinc.com>
2023-03-27 20:07:54 +0530

    Removed html cache busting in favour of versioned cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,7 @@
-const CACHE_NAME = 'doodoo-cache';
+// Set the cache name and version
+const CACHE_NAME = 'ipas-cache-v1';
+
+// Set the list of URLs to cache
 const urlsToCache = [
     '/',
     '/js/pocketbase.umd.js',
@@ -7,25 +10,37 @@ const urlsToCache = [
     'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css',
 ];
 
+// Listen to the install event
 self.addEventListener('install', event => {
+    // Wait until the cache is opened and the URLs are cached
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
     );
 });
 
+// Listen to the activate event
+self.addEventListener('activate', (event) => {
+    // Wait until the old caches are deleted
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((oldCacheName) => {
+                    // Delete the old cache if it's not the current cache
+                    if (oldCacheName !== CACHE_NAME) {
+                        return caches.delete(oldCacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Listen to the fetch event
 self.addEventListener('fetch', (event) => {
-    // Check if the request is for an HTML file
-    if (event.request.headers.get('Accept').includes('text/html')) {
-        // Add a cache-busting URL parameter
-        const url = new URL(event.request.url);
-        url.searchParams.set('cache-bust', Date.now());
-        const cacheBustedRequest = new Request(url, event.request);
-        event.respondWith(fetch(cacheBustedRequest));
-    } else {
-        event.respondWith(
-            caches.match(event.request)
-                .then(response => response || fetch(event.request))
-        );
-    }
+    // Respond with the cached resource or fetch it from the network
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
